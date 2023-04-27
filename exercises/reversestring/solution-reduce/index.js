a2215c6a9ed22e951e08a98b60577c655a198584
Simone Taeggi <startae14@gmail.com>
2023-04-28 00:26:53 +0200

    reversestring solution with array reduce method

---
+++ exercises/reversestring/solution-reduce/index.js
@@ -0,0 +1,16 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+  const stringArray = str.split("");
+  return stringArray.reduce((acc, currentChar) => {
+    return currentChar + acc;
+  }, '');
+}
+
+module.exports = reverse;
