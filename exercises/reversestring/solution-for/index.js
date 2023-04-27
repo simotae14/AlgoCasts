438793906af663f8739d71dc9d93c77efabd477f
Simone Taeggi <startae14@gmail.com>
2023-04-28 00:26:22 +0200

    reversestring solution with for

---
+++ exercises/reversestring/solution-for/index.js
@@ -0,0 +1,17 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+  let reversed = '';
+  for (let char of str) {
+    reversed = char + reversed;
+  }
+  return reversed;
+}
+
+module.exports = reverse;
