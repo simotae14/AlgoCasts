53ca4656b42c30b5531c4e85239eda6a465155c4
Simone Taeggi <startae14@gmail.com>
2023-04-29 00:16:46 +0200

    palindrome solution reverse

--- exercises/palindrome/solution-reverse/index.js
+++ exercises/palindrome/solution-reverse/index.js
@@ -8,7 +8,10 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-  return str.split('').reverse().join('') === str;
+  return str
+    .split('')
+    .reverse()
+    .join('') === str;
 }
 
 module.exports = palindrome;
