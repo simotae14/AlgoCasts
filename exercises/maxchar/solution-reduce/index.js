43eb2ef947736abfefa06b150b3bb55521a7eae5
Simone Taeggi <startae14@gmail.com>
2023-04-29 00:18:52 +0200

    maxchar solution with reduce

---
+++ exercises/maxchar/solution-reduce/index.js
@@ -0,0 +1,28 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+  const strArray = str.split('');
+  
+  const objStr = strArray.reduce((acc, item) => {
+    acc[item] = acc[item] ? acc[item] + 1 : 1;
+    return acc;
+  }, {});
+  const maxObj = Object.keys(objStr).reduce((maxObj, key) => {
+    return objStr[key] > maxObj.max ? {
+      max: objStr[key],
+      maxChar: key
+    } : maxObj;
+  }, {
+    max: 0,
+    maxChar: ''
+  });
+
+  return maxObj.maxChar;
+}
+
+module.exports = maxChar;
