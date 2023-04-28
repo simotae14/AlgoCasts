96e72a0fcc9cb72013f2f8506a9a7aca66e7c35e
Simone Taeggi <startae14@gmail.com>
2023-04-29 00:19:44 +0200

    fizzbuzz problem starter code

--- exercises/fizzbuzz/starter/index.js
+++ exercises/fizzbuzz/starter/index.js
@@ -1,5 +1,3 @@
-// --- Directions
-// Write a program that console logs the numbers
 // from 1 to n. But for multiples of three print
 // “fizz” instead of the number and for the multiples
 // of five print “buzz”. For numbers which are multiples
