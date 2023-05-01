852e2b645cbe47ce1394f649b813d1d05cbcb351
Simone Taeggi <startae14@gmail.com>
2023-05-01 18:40:07 +0200

    fibonacci solution iterative with for loop

---
+++ exercises/fib/solution-iterative/index.js
@@ -0,0 +1,20 @@
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceeding two.
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+
+function fib(n) {
+  const result = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    result.push(result[i - 2] + result[i - 1]);
+  }
+  return result[n];
+}
+
+module.exports = fib;
+// Runtime Complexity = O(n)
