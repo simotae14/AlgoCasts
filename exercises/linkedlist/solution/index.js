4b960be8965513b4c9ff9ec6481bf99463649d2c
Simone Taeggi <startae14@gmail.com>
2023-05-02 00:46:33 +0200

    LinkedList Data Structure, implement LinkedList size method

--- exercises/linkedlist/solution/index.js
+++ exercises/linkedlist/solution/index.js
@@ -18,6 +18,16 @@ class LinkedList {
     const node = new Node(data, this.head);
     this.head = node;
   }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+    return counter;
+  }
 }
 
 module.exports = { Node, LinkedList };
