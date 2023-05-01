4cc5414e2449910d3d007c03916b993b7aedae8f
Simone Taeggi <startae14@gmail.com>
2023-05-01 19:53:15 +0200

    fibonacci solution with recursive memoized function

--- exercises/fib/solution-memoize-recursion/index.js
+++ exercises/fib/solution-memoize-recursion/index.js
@@ -8,7 +8,21 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    
+    return result;
+  }
+}
+
+function slowFib(n) {
   // basic scenario
   if (n < 2) {
     return n;
@@ -16,6 +30,8 @@ function fib(n) {
 
   return fib(n - 1) + fib(n - 2);
 }
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
 // Runtime Complexity = O(2^n)
