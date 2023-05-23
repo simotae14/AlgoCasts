0c1a7c5386c25f49ef1def70ec3762c21fee9689
Simone Taeggi <startae14@gmail.com>
2023-05-23 17:37:44 +0200

    Bubble Sort Algorithm implementation

---
+++ exercises/sorting/solution/index.js
@@ -0,0 +1,31 @@
+// --- Directions
+// Implement bubbleSort, selectionSort, and mergeSort
+
+function bubbleSort(arr) {
+  // Implement Bubble sort
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const lesser = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
+  // return the sorted array
+  return arr;
+}
+
+function selectionSort(arr) {
+
+}
+
+function mergeSort(arr) {
+
+}
+
+function merge(left, right) {
+
+}
+
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
