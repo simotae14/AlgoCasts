11c951e8eeba7805f1384f5315038eea112c7ff7
Simone Taeggi <startae14@gmail.com>
2023-05-23 19:27:09 +0200

    Selection Sort Algorithm implementation

--- exercises/sorting/solution/index.js
+++ exercises/sorting/solution/index.js
@@ -17,7 +17,22 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
-
+  // implement Selection sort
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
+    if (indexOfMin !== i) {
+      let lesser = arr[indexOfMin];
+      arr[indexOfMin] = arr[i];
+      arr[i] = lesser; 
+    }
+  }
+  // return the sorted array
+  return arr;
 }
 
 function mergeSort(arr) {
